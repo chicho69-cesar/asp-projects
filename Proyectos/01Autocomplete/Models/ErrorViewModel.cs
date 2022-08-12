baffa98c3ef5a338b98a2a2d4a33ebaa7483e7e5
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-12 18:27:11 -0500

    Init Autocomplete

---
+++ Proyectos/01Autocomplete/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace _01Autocomplete.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
