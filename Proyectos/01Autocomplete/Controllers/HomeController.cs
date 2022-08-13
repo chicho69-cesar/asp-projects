a1c61a7ced0ec12c4addc58742def9d1d89eb27a
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-12 19:34:55 -0500

    Finish autocomplete

--- Proyectos/01Autocomplete/Controllers/HomeController.cs
+++ Proyectos/01Autocomplete/Controllers/HomeController.cs
@@ -1,19 +1,48 @@
 ﻿using _01Autocomplete.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace _01Autocomplete.Controllers {
     public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
+        private readonly string _connectionString;
 
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
+        public HomeController(IConfiguration configuration) {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
+        [HttpGet]
         public IActionResult Index() {
             return View();
         }
 
+        [HttpGet]
+        public JsonResult SearchProducts(string searched) {
+            List<Search> searchList = new List<Search>();
+
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("sp_search_products", connection);
+            command.Parameters.AddWithValue("search", searched);
+            command.CommandType = CommandType.StoredProcedure;
+
+            using var results = command.ExecuteReader();
+
+            while (results.Read()) {
+                searchList.Add(new Search {
+                    value = Convert.ToInt32(results["id"]),
+                    label = results["Text"].ToString(),
+                    price = Convert.ToDecimal(results["price"]),
+                    description = results["description"].ToString()
+                });
+            }
+
+            return Json(searchList);
+        }
+
+        [HttpGet]
         public IActionResult Privacy() {
             return View();
         }
---
+++ Proyectos/01Autocomplete/Models/Search.cs
@@ -0,0 +1,8 @@
+﻿namespace _01Autocomplete.Models {
+    public class Search {
+        public int value { get; set; }
+        public string label { get; set; }
+        public decimal price { get; set; }
+        public string description { get; set; }
+    }
+}
