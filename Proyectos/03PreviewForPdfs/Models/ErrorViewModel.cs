1e90694c45bbbb39a75933a12f1660ecad965f48
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-13 11:25:13 -0500

    Cancel PreviewForPdfs because the weight of the libraries

--- Proyectos/03PreviewForPdfs/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace _03PreviewForPdfs.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
