3cd9cda50456e46b1cdb9ef703d9538a5d2b06d6
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-14 16:28:34 -0500

    Init the client for de API to upload files

---
+++ Proyectos/04APIClient/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace _04APIClient.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
