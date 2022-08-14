4ff150d9152ba6e44e0d42dd43340726cd4ada32
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-14 16:53:34 -0500

    Finish the client for the API to save images

--- Proyectos/04APIClient/Controllers/HomeController.cs
+++ Proyectos/04APIClient/Controllers/HomeController.cs
@@ -1,21 +1,41 @@
 ﻿using _04APIClient.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 namespace _04APIClient.Controllers {
     public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
+        public HomeController() {}
 
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
-        }
-
+        [HttpGet]
         public IActionResult Index() {
+            ViewBag.Message = TempData["message"];
             return View();
         }
 
+        [HttpGet]
         public IActionResult Privacy() {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(string description, IFormFile file) {
+            var client = new HttpClient();
+
+            using var multipartFormContent = new MultipartFormDataContent();
+
+            var fileStreamContent = new StreamContent(file.OpenReadStream());
+            fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            
+            multipartFormContent.Add(new StringContent(description), name: "Description");
+            multipartFormContent.Add(fileStreamContent, name: "DocumentFile", fileName: file.FileName);
+
+            var request = await client.PostAsync("https://localhost:7204/api/Documents/upload", multipartFormContent);
+            var response = request.Content.ReadAsStringAsync();
+
+            TempData["message"] = "Imagen guardada con exito !!!";
+
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
