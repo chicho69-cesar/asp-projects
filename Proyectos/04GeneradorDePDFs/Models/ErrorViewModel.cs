f70f61bd3d5f10bfe29ffa7e156a08f94b5aa464
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-14 14:32:32 -0500

    Init Generador de documentos PDF

---
+++ Proyectos/04GeneradorDePDFs/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace _04GeneradorDePDFs.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
