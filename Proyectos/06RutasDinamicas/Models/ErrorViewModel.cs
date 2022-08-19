f1549b77ef56a9f5327c478ad3cad30ff3d094a7
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-18 23:47:45 -0500

    Init Parametros Catch All

---
+++ Proyectos/06RutasDinamicas/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace _06RutasDinamicas.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
